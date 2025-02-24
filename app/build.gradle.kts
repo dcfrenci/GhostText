404720faf8bfe2ccbfec6ce9ed062c65a7af74fb
dcfrenci <dcfrenci@gmail.com>
2025-02-24 23:12:53 +0100

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
